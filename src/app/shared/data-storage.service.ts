d4dc9ad35e121790503b5e38be07ea1020aef788
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-12-22 15:02:02 -0500

    I didn't make many changes, because I was trying to figure out what was wrong, the error was supposedly in my data storage service, but it was actually in the database, somehow all my projects in there got deleted, so there was nothing to pull out from there. Note to myself: -BEFORE YOU MESS WITH THE CODE. REMEMBER TO CHECK THE DATABASE!!

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,25 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
-import { ProjectModel } from '../projects/project.model';
-import { ProjectService } from '../projects/project.service';
-import { AuthService } from '../auth/auth.service';
+import { ProjectModel } from "../projects/project.model";
+import { ProjectService } from "../projects/project.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(
-    private http: HttpClient,
-    private projectService: ProjectService,
-    private authService: AuthService
-  ) {}
+  constructor(private http: HttpClient, private projectService: ProjectService) {}
 
   storeProjects() {
-    const recipes = this.projectService.getProjects();
+    const projects = this.projectService.getProjects();
     this.http
       .put(
-        'https://ng-course-recipe-book-56390.firebaseio.com/recipes.json',
-        recipes
+        'https://ng-course-recipe-book-56390.firebaseio.com/projects.json',
+        projects
       )
       .subscribe(response => {
         console.log(response);
@@ -27,27 +22,22 @@ export class DataStorageService {
   }
 
   fetchProjects() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<ProjectModel[]>(
-          'https://ng-course-recipe-book-56390.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map(projects => {
-        return projects.map(project => {
-          return {
-            ...project,
-            tasks: project.tasks ? project.tasks : []
-          };
-        });
-      }),
-      tap(projects => {
-        this.projectService.setProjects(projects);
-      })
-    );
+    return this.http
+      .get<ProjectModel[]>(
+        'https://ng-course-recipe-book-56390.firebaseio.com/projects.json'
+  )
+      .pipe(
+        map(projects => {
+          return projects.map(project => {
+            return {
+              ...project,
+              ingredients: project.tasks ? project.tasks : []
+            };
+          });
+        }),
+        tap(projects => {
+          this.projectService.setProjects(projects);
+        })
+      )
   }
 }
